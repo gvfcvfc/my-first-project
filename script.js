b185c18ba70a2ce637a3180430179102292b3941
mohannad al refai <your@email.com>
2025-07-25 15:49:18 -0400

    this is the full project

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
- function getJoke() {
-      // 1. Call the API
-      fetch("https://official-joke-api.appspot.com/random_joke")
-        .then(response => response.json()) // 2. Convert the response to JSON
-        .then(data => {
-          // 3. Use the data
-          document.getElementById("setup").innerText = data.setup;
-          document.getElementById("punchline").innerText = data.punchline;
-        })
-        .catch(error => {
-          // 4. Handle errors
-          console.error("Error fetching joke:", error);
-        });
+ async function getJoke() {
+    try{
+    const response = await fetch("https://official-joke-api.appspot.com/random_joke")
+     if (!response.ok) {
+      throw new Error("⚠️ Network response was not OK");}
+    const data = await response.json();
+    document.getElementById("setup").innerText = data.setup;
+    document.getElementById("punchline").innerText = data.punchline;
+   console.log(data); }
+    catch (error) {document.getElementById("error").innerText = "⚠️ Error: " + error.message;
+};
+
+    
     }
